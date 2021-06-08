428a7fc95bb8b0f14734368d9a2a56279ecb5f45
Adan Florez <adan.florezb@gmail.com>
2021-06-08 02:06:47 -0500

    feat: input component

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
     <Loading v-if="isLoading" />
-    <FooterButtons v-else />
+    <template v-else>
+      <div class="grid grid-cols-8 gap-4 pt-9">
+        <div class="col-start-2 lg:col-start-3 col-span-6 lg:col-span-4">
+          <UIInput placeholder="Search" @input="search">
+            <SearchIcon
+              class="m-4 fill-current text-default-slate"
+              slot="icon"
+            />
+          </UIInput>
+        </div>
+      </div>
+      <FooterButtons />
+    </template>
   </div>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
+// Components
 import FooterButtons from "@/components/common/FooterButtons.vue";
 import Loading from "@/components/common/Loading.vue";
-import Vue from "vue";
+import UIInput from "@/components/ui/UIInput.vue";
+// Icons
+import SearchIcon from "@/assets/img/search.svg";
 
 export default Vue.extend({
-  components: { Loading, FooterButtons },
+  components: { Loading, FooterButtons, UIInput, SearchIcon },
   data() {
     return {
       isLoading: true,
@@ -21,6 +37,11 @@ export default Vue.extend({
     setTimeout(() => {
       this.isLoading = false;
     }, 2000);
+  },
+  methods: {
+    search(word: String): void {
+      console.log(word);
+    },
   },
 });
 </script>
