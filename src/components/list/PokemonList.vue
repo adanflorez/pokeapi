498b7a9215e4d4867906ba4b204148400fc16eb5
Adan Florez <adan.florezb@gmail.com>
2021-06-09 00:50:49 -0500

    feat: pokemon listing in props

--- src/components/list/PokemonList.vue
+++ src/components/list/PokemonList.vue
@@ -5,10 +5,17 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropOptions } from "vue";
 import PokemonItem from "@/components/list/PokemonItem.vue";
+import { Pokemon } from "@/interfaces/pokemon";
 
 export default Vue.extend({
+  props: {
+    pokemons: {
+      type: Array,
+      default: () => []
+    } as PropOptions<Array<Pokemon>>
+  },
   components: {
     PokemonItem,
   },
