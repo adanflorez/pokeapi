20b49e4ce79d1739fa5aef46adc50f9673d8f61c
Adan Florez <adan.florezb@gmail.com>
2021-06-09 00:17:05 -0500

    feat: axios was installed

---
+++ src/axios.ts
@@ -0,0 +1,17 @@
+// axios
+import axios from 'axios'
+
+const urlApi = process.env.VUE_APP_BASE_URL
+console.log('api', urlApi);
+const api = axios.create({
+  baseURL: urlApi
+})
+
+// Set Response Interceptor
+api.interceptors.response.use(function (response) {
+  return response
+}, function (error) {
+  return Promise.reject(error)
+})
+
+export default api
