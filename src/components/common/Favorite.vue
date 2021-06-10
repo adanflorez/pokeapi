301ea0ee32a62cb71db02f7ea1c50171ab773d51
Adan Florez <adan.florezb@gmail.com>
2021-06-10 02:32:49 -0500

    feat: favorite component

---
+++ src/components/common/Favorite.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="
+      rounded-full
+      h-11
+      w-11
+      bg-ligth-smoke
+      flex
+      justify-center
+      items-center
+      cursor-pointer
+      text-default-slate
+      hover:text-default-secondary
+      cursor-pointer
+    "
+  >
+    <StarIcon
+      class="fill-current hover:text-default-secondary"
+      :class="{ 'text-default-secondary': isFavorite }"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import StarIcon from "@/assets/img/star.svg";
+
+export default Vue.extend({
+  components: {
+    StarIcon,
+  },
+  props: {
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
+
+<style scoped>
+</style>
